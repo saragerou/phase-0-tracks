43b3e6f43dc137109927371d81fe126fa6d59403
saragerou <saragerou@gmail.com>
2016-12-24 10:01:54 -0600

    Add puppy.rb class file

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# CLASS
+# Puppy
+
+# CHARACTERISTICS
+# Size: varies
+# Fur length: varies
+# Friendly: yes
+# Legs: 4
+# Eyes: 2
+# Nose: 1
+# Slobbery: true
+
+# BEHAVIOR
+# Sit
+# Stay
+# Play dead
+# Fetch
+# Snuggle
