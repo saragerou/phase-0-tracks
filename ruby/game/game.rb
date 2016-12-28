856522910963e1b6d6462d0470fed1296f9009d7
saragerou <saragerou@gmail.com>
2016-12-28 12:36:02 -0600

    Add initial folders for game and student-drills

---
+++ ruby/game/game.rb
@@ -0,0 +1,13 @@
+# One user can enter a word (or phrase, if you would like your 
+# game to support that), and another user attempts to guess the word.
+# Guesses are limited, and the number of guesses available is related 
+# to the length of the word.
+# Repeated guesses do not count against the user.
+# The guessing player receives continual feedback on the current state 
+# of the word. So if the secret word is "unicorn", the user will start 
+# out seeing something like "_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" 
+# after the user enters a guess of "c".
+# The user should get a congratulatory message if they win, and a 
+# taunting message if they lose.
+
+# 
