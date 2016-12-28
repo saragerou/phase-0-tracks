82bc6917086d8baba5d7f765a9829675a6e3269c
saragerou <saragerou@gmail.com>
2016-12-28 14:20:02 -0600

    Update student-drills iteration.rb completely challenges

--- ruby/student-drills/iteration.rb
+++ ruby/student-drills/iteration.rb
@@ -17,9 +17,8 @@ p zombie_apocalypse_supplies.sort!
 # 3. Create a method to see if a particular item (string) is in the 
 # zombie_apocalypse_supplies, don't use any built in methods.
 # For instance: are boots in your list of supplies?
-# ----
-def find(item)
-  if zombie_apocalypse_supplies.index("")
+def find(str)
+  if str == zombie_apocalypse_supplies.index
     true
   end
 end
@@ -27,13 +26,21 @@ end
 
 # 4. You can't carry too many things, you've only got room in your pack for 5. 
 # Remove items in your zombie_apocalypse_supplies in any way you'd like, leaving only 5.
-# ----
+zombie_apocalypse_supplies.delete("batteries")
+zombie_apocalypse_supplies.delete_if { |item| item == "rations" }
+zombie_apocalypse_supplies.shift
+p zombie_apocalypse_supplies
 
 # 5. You found another survivor! This means you can combine your supplies. 
 # Create a new combined supplies list out of your zombie_apocalypse_supplies 
 # and their supplies below. You should get rid of any duplicate items. 
 other_survivor_supplies = ["warm clothes", "rations", "compass", "camp stove", "solar battery", "flashlight"]
-# ----
+
+new_array = []
+new_array << zombie_apocalypse_supplies + other_survivor_supplies
+new_array = new_array.flatten!
+new_array = new_array.uniq
+p new_array
 
 # A Hash is a dictionary-like collection of unique keys and their values. 
 # Also called associative arrays, they are similar to Arrays, but where an 
@@ -46,24 +53,29 @@ extinct_animals = {"Tasmanian Tiger" => 1936, "Eastern Hare Wallaby" => 1890, "D
 
 # 1. Iterate through extinct_animals, printing each key/value pair with a dash in 
 # between the key and value, and an asterisk between each pair
-# ----
+extinct_animals.each { |print| puts "#{print}*"}
 
 # 2. Keep only animals in extinct_animals if they were extinct before the year 
 # 2000, don't use any special methods from the Hash module.
-# ----
+extinct_animals.delete_if {|key, value|  value < 2000 }
+p extinct_animals
 
 # 3. Our calculations were completely off, turns out all of those animals went 
 # extinct 3 years before the date provided. Update the values in extinct_animals 
 # so they accurately reflect what year the animal went extinct.
-# ----
+extinct_animals.update(extinct_animals){|key,value| (value - 3)}
+p extinct_animals
 
 # 4. You've heard that the following animals might be extinct, but you're not sure. 
 # Check if they're included in extinct_animals, one by one:
 # "Andean Cat"
 # "Dodo"
 # "Saiga Antelope"
-# ----
+extinct_animals.has_key?("Andean Cat")
+extinct_animals.has_key?("Dodo")
+extinct_animals.has_key?("Saiga Antelope")
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct! Remove 
 # them from extinct_animals and return the key value pair as a two item array.
-# ----
+extinct_animals.delete("Passenger Pigeon")
+p extinct_animals.to_a
