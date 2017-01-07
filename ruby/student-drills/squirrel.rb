70cf2271c8c69d25f5f4227021a330b0bea3e3f6
saragerou <saragerou@gmail.com>
2017-01-07 09:50:14 -0600

    Add method updates to squirrel.rb

--- ruby/student-drills/squirrel.rb
+++ ruby/student-drills/squirrel.rb
@@ -32,9 +32,43 @@
 # ---
 
 
+# Brrr, it's cold out there! We're going to follow along with one of North America's most common mammals that we see all year round: the squirrel.
+#
+# Create a Squirrel class that can be initialized with a color and location. There are gray, red, black, and even albino squirrels and they live in locations like the city, state, woods, in town, whatever you feel like.
+#
+# For instance: one squirrel could be a gray squirrel who lives in the boonies, another instance of a squirrel could be red who lives in Manhattan. Initialize a couple of different squirrels to demonstrate this functionality.
+# ----
+
+# Our squirrels might want to find a new home base if too many humans or predators move in, but they can never change their color. Make the color readable from outside the class, and make the location readable and writeable.
+# ----
+
+# Contrary to popular belief, squirrels don't hibernate during the winter, they just sleep a lot!
+#
+# Create two instance methods: `wake_up` and `sleep`. These should update an instance variable in order to let us know if the squirrel is awake or not. Every squirrel should start out asleep when they are initialized.
+# ----
+
+# In preparation for winter, squirrels and other small animals hide nuts to get them through the winter. Nuts are hidden in various locations.
+#
+# Create a `hoard_locations` data structure that will track two things: the location of a particular hoard (e.g. "knot hole" or "barn attic"), and how many nuts are stored there (e.g. 6). The `hoard_locations` should start out empty.
+# ----
+
+# During the summer and fall, the squirrel has to hide their nuts. Create a `store_food` instance method that takes a hoard location and a number of nuts, and stores this in the `hoard_locations` data structure. If the hoard location already exists, this method should add to the existing number of nuts there. If the hoard location doesn't exist, it should create a new entry.
+# ---
+
+# Every day during the winter, the squirrel will wake up and find one of his nut stores. Squirrels don't have a great memory so the squirrel will just find one of the stores and eat three of the nuts.
+#
+# Create an instance method called `eat_something` that will wake the squirrel up, choose a hoard location at random, remove a random number of nuts for the squirrel to eat, and then have the squirrel go back to sleep.
+# ---
+
+# Squirrels hide their nuts in holes in the ground, but sometimes forget about them. Sad for the squirrel, but these can sprout into trees so yay!
+#
+# Create an instance method called `forget` that deletes a random hoard location from the squirrel's hoard locations and returns a string telling us that a new tree has sprouted.
+# ---
+
+
 class Squirrel
 
-attr_reader :color
+attr_reader :color, :arr
 attr_accessor :location
 
   def initialize (color, location)
@@ -48,20 +82,41 @@ attr_accessor :location
   
   def wake_up
     @sleep = false
+    @wake_up = true
   end
 
   def sleep
+    @sleep = true
+    @wake_up = false
   end
 
-  def hide
+  def store_food(new_location, int)
+    @hoard_locations[new_location]
+    @hoard_locations[new_location.to_sym] = int
+    p @hoard_locations
   end
   
-  def store_food(hoard_locations, nuts_number)
-    @hoard locations = {locations, nuts_number}
+  def eat_something
+    p @sleep
+    @sleep = false
+    p @sleep
+    @arr = @hoard_locations.to_a.shuffle
+    @arr[0][1] = @arr [0][1].to_i - 3
+    p @arr[0]
+    @hoard_locations = @arr.to_h
+    p @hoard_locations
+    @sleep = true
+    p @sleep
   end
   
   def forget
+    p @hoard_locations
+    @arr = @hoard_locations.to_a
+    @arr.delete_at(rand(@arr.length))
+    p @arr
     puts "A new tree has sprouted!"
+    @hoard_locations = @arr.to_h
+    p @hoard_locations
   end
   
 end
@@ -71,3 +126,7 @@ squirrel = Squirrel.new("gray", "country")
 squirrel = Squirrel.new("albino", "woods")
 squirrel = Squirrel.new("purple", "town")
 squirrel = Squirrel.new("brown", "tree")
+squirrel.store_food("hole", 5)
+squirrel.store_food("pool_table", 19)
+squirrel.eat_something
+squirrel.forget
